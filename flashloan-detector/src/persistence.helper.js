be0158af8d6e0d95ebee5837ee57785b92baf6a2
Roberto Cantu <Roberto_Cantu@live.com>
2023-01-09 21:15:18 -0600

    feat: Update logic to better handle fetch response

--- flashloan-detector/src/persistence.helper.js
+++ flashloan-detector/src/persistence.helper.js
@@ -5,34 +5,36 @@ const { existsSync, readFileSync, writeFileSync } = require("fs");
 const { Buffer } = require("node:buffer");
 
 class PersistenceHelper {
+  databaseUrl;
+
+  constructor(dbUrl) {
+    this.databaseUrl = dbUrl;
+  }
 
   async persist(value, key) {
     const valueToPersist = Buffer.from(value.toString());
-    const hasLocalNode = await process.env.hasOwnProperty("LOCAL_NODE"); // MOCK INSTANCE NEEDS AWAIT :/
+    const hasLocalNode = process.env.hasOwnProperty("LOCAL_NODE");
+    // console.log(`hasLocalNode: ${hasLocalNode}`);
     if (!hasLocalNode) {
-      // fetchJwt() erroring out when running locally, (not test suite)
-      // though the docs mention it should return a mock value
-      // source: https://docs.forta.network/en/latest/sdk/#fetchjwt
-      // error: "Could not resolve host 'forta-jwt-provider'.
-      // This url host can only be resolved inside of a running scan node"
       const token = await fetchJwt({});
+      // console.log(`token: ${JSON.stringify(token)}`);
 
-      // When logged, headers is an empty object
       const headers = new Headers({ Authorization: `Bearer ${token}` });
       try {
-        const response = await (
-          await fetch(`https://research.forta.network/database/bot/${key}`, {
-            method: "POST",
-            headers,
-            body: valueToPersist,
-          })
-        ).json();
+        const response = await fetch(`${this.databaseUrl}${key}`, {
+          method: "POST",
+          headers: headers,
+          body: valueToPersist,
+        });
 
-        if (response) {
+        console.log(`response.ok: ${JSON.stringify(response.ok)}`);
+
+        if (response.ok) {
+          console.log(`succesfully persisted ${value} to database`);
           return;
         }
-      } catch {
-        console.log(`failed to persist ${value} to database`);
+      } catch (e) {
+        console.log(`failed to persist ${value} to database. Error: ${e}`);
       }
     } else {
       // Persist locally
@@ -43,33 +45,34 @@ class PersistenceHelper {
 
   async load(key) {
     const hasLocalNode = process.env.hasOwnProperty("LOCAL_NODE");
+    // console.log(`hasLocalNode: ${hasLocalNode}`);
     if (!hasLocalNode) {
-      // fetchJwt() erroring out when running locally,
-      // though the docs mention it should return a mock value
-      // source: https://docs.forta.network/en/latest/sdk/#fetchjwt
-      // error: "Could not resolve host 'forta-jwt-provider'.
-      // This url host can only be resolved inside of a running scan node"
-      // const token = await fetchJwt({});
-      const token = { token: "jwt-string" }; // For testing
+      const token = await fetchJwt({});
+      // console.log(`token: ${JSON.stringify(token)}`);
 
-      // When logged, headers is an empty object
       const headers = new Headers({ Authorization: `Bearer ${token}` });
-      // This instance of fetching from the DB returns a message of:
-      // {"message":"unauthorized"}, expectedly. Could be from either
-      // the mock Jwt or headers being an empty object, possibly both
-      const response = await (await fetch(`https://research.forta.network/database/bot/${key}`, { headers })).json();
+      try {
+        const response = await fetch(`${this.databaseUrl}${key}`, { headers });
+        // console.log(`response.ok: ${JSON.stringify(response.ok)}`);
 
-      // Response.body should be a string, so this should work
-      if (response.ok && response.body.length > 0) {
-        const bufferString = (await response.buffer()).toString();
-        return JSON.parse(bufferString);
-      } else {
-        console.log(`${key} has no database entry`);
-        // If this is the first bot instance that is deployed,
-        // the database will not have data to return,
-        // thus return zero to assign value to the variables
-        // necessary
-        return 0;
+        if (response.ok) {
+          const data = await response.json();
+          // console.log(`data: ${JSON.stringify(data)}`);
+          const bufferString = (await data.buffer()).toString();
+          // console.log(`bufferString: ${bufferString}`);
+          console.log(`successfully fetched value from database`);
+          return JSON.parse(bufferString);
+        } else {
+          console.log(`${key} has no database entry`);
+          // If this is the first bot instance that is deployed,
+          // the database will not have data to return,
+          // thus return zero to assign value to the variables
+          // necessary
+          return 0;
+        }
+      } catch (e) {
+        console.log(`Error in fetching data.`);
+        throw e;
       }
     } else {
       // Checking if it exists locally
