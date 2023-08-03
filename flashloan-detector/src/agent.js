9f00c9c6585e0313d8f136b691a7e638ca4b7973
Mani <manichandra.teja@gmail.com>
2023-08-04 00:42:46 +0530

    chore: removed unnecessary console.logs

--- flashloan-detector/src/agent.js
+++ flashloan-detector/src/agent.js
@@ -191,7 +191,6 @@ function provideHandleTransaction(helper, getFlashloans, provider) {
           // Check the profit of the initiator if not on mainnet
           // or if no traces only during the last flashloan
         } else if (flashloanIndex === numOfFlashloans - 1) {
-          console.log("Seventh If");
           const tokenProfits = helper.calculateTokenProfits(transferEvents, initiator);
           Object.entries(tokenProfits).forEach(([address, profit]) => {
             if (!totalTokenProfits[address]) totalTokenProfits[address] = helper.zero;
