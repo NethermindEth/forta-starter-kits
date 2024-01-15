ab3030e06f32478db2f94be3baba8cfbdbb57aa0
Roberto Cantu <Roberto_Cantu@live.com>
2024-01-15 15:50:09 -0600

    feat: Swapping OUT of flashloaned token check to new recipient

--- flashloan-detector/src/detectors/uniswap-v3-detector.js
+++ flashloan-detector/src/detectors/uniswap-v3-detector.js
@@ -1,6 +1,7 @@
 const { ethers, getEthersProvider } = require("forta-agent");
 
 const functionSignature = "function flash(address recipient, uint256 amount0, uint256 amount1, bytes calldata data)";
+const swapAbi = "function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes data)"
 
 const ABI = [
   "function token0() public view returns (address token)",
@@ -10,24 +11,41 @@ const ABI = [
 module.exports = {
   getUniswapV3Flashloan: async (txEvent) => {
     const flashWithdraws = txEvent.filterFunction(functionSignature);
+    const assetSwaps = txEvent.filterFunction(swapAbi);
 
     const flashloans = await Promise.all(
       flashWithdraws.map(async (withdraw) => {
-        const { recipient, amount0, amount1 } = withdraw.args;
-        const { address } = withdraw;
+        const { recipient: flashloanRecipient, amount0, amount1 } = withdraw.args;
+        const { address: flashloanAddress } = withdraw;
+        let swapRecipient;
 
         // Get the correct amount and asset address
         const tokenIndex = amount0.gt(ethers.constants.Zero) ? 0 : 1;
         const amount = tokenIndex === 0 ? amount0 : amount1;
         const tokenFnCall = tokenIndex === 0 ? "token0" : "token1";
 
-        const contract = new ethers.Contract(address, ABI, getEthersProvider());
+        const contract = new ethers.Contract(flashloanAddress, ABI, getEthersProvider());
         const asset = await contract[tokenFnCall]();
+
+        await Promise.all(assetSwaps.map(async (swap) => {
+          const { recipient: swapReceiver, zeroForOne } = swap.args;
+          const { address: swapAddress } = swap;
+
+          // Check if there was a `swap` in the same
+          // UniswapV3 Pool in the same txn
+          if (swapAddress != flashloanAddress) return;
+
+          // Check if the `swap` was swapping OUT of the
+          // flashloaned token into the pool's other token
+          if ((tokenIndex === 0 && zeroForOne === true) || (tokenIndex === 1 && zeroForOne === false)) {
+            swapRecipient = swapReceiver;
+          };
+        }));
 
         return {
           asset: asset.toLowerCase(),
           amount,
-          account: recipient.toLowerCase(),
+          account: !swapRecipient ? flashloanRecipient.toLowerCase() : swapRecipient.toLowerCase()
         };
       })
     );
