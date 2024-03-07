8baf10a397d8b162c6a39fa17357d72fe1e9e503
Vxatz <vxatzistratis@gmail.com>
2024-03-07 14:28:30 +0200

    chore: Check event instead of function for Uniswap V3 flashloan

--- flashloan-detector/src/detectors/uniswap-v3-detector.js
+++ flashloan-detector/src/detectors/uniswap-v3-detector.js
@@ -1,7 +1,10 @@
 const { ethers, getEthersProvider } = require("forta-agent");
 
-const functionSignature = "function flash(address recipient, uint256 amount0, uint256 amount1, bytes calldata data)";
-const swapAbi = "function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes data)"
+const FLASH_EVENT_ABI = [
+  "event Flash(address indexed sender, address indexed recipient, uint256 amount0, uint256 amount1, uint256 paid0, uint256 paid1)",
+];
+const swapAbi =
+  "function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes data)";
 
 const ABI = [
   "function token0() public view returns (address token)",
@@ -10,13 +13,13 @@ const ABI = [
 
 module.exports = {
   getUniswapV3Flashloan: async (txEvent) => {
-    const flashWithdraws = txEvent.filterFunction(functionSignature);
+    const flashEvents = txEvent.filterLog(FLASH_EVENT_ABI);
     const assetSwaps = txEvent.filterFunction(swapAbi);
 
     const flashloans = await Promise.all(
-      flashWithdraws.map(async (withdraw) => {
-        const { recipient: flashloanRecipient, amount0, amount1 } = withdraw.args;
-        const { address: flashloanAddress } = withdraw;
+      flashEvents.map(async (flash) => {
+        const { recipient: flashloanRecipient, amount0, amount1 } = flash.args;
+        const { address: flashloanAddress } = flash;
         let swapRecipient;
 
         // Get the correct amount and asset address
@@ -28,26 +31,28 @@ module.exports = {
         const asset = await contract[tokenFnCall]();
 
         if (assetSwaps.length > 0) {
-          await Promise.all(assetSwaps.map(async (swap) => {
-            const { recipient: swapReceiver, zeroForOne } = swap.args;
-            const { address: swapAddress } = swap;
+          await Promise.all(
+            assetSwaps.map(async (swap) => {
+              const { recipient: swapReceiver, zeroForOne } = swap.args;
+              const { address: swapAddress } = swap;
 
-            // Check if there was a `swap` in the same
-            // UniswapV3 Pool in the same txn
-            if (swapAddress != flashloanAddress) return;
+              // Check if there was a `swap` in the same
+              // UniswapV3 Pool in the same txn
+              if (swapAddress != flashloanAddress) return;
 
-            // Check if the `swap` was swapping OUT of the
-            // flashloaned token into the pool's other token
-            if ((tokenIndex === 0 && zeroForOne === true) || (tokenIndex === 1 && zeroForOne === false)) {
-              swapRecipient = swapReceiver;
-            };
-          }));
+              // Check if the `swap` was swapping OUT of the
+              // flashloaned token into the pool's other token
+              if ((tokenIndex === 0 && zeroForOne === true) || (tokenIndex === 1 && zeroForOne === false)) {
+                swapRecipient = swapReceiver;
+              }
+            })
+          );
         }
 
         return {
           asset: asset.toLowerCase(),
           amount,
-          account: !swapRecipient ? flashloanRecipient.toLowerCase() : swapRecipient.toLowerCase()
+          account: !swapRecipient ? flashloanRecipient.toLowerCase() : swapRecipient.toLowerCase(),
         };
       })
     );
