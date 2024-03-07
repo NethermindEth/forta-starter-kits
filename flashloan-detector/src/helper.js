8a972dcae8654d95a1770cc1b1ed8c8ebb89b968
Vxatz <vxatzistratis@gmail.com>
2024-03-07 14:22:18 +0200

    chore: Add getContractCreator and addWethBurnProfitIfApplicable helper methods

--- flashloan-detector/src/helper.js
+++ flashloan-detector/src/helper.js
@@ -10,14 +10,40 @@ const ABI = ["function decimals() external view returns (uint8)"];
 // sDAI on Ethereum mainnet, 0x83F20F44975D03b1b09e64809B757c47f942BEeA,
 // does not emit a `Transfer` event when `burn`ing tokens.
 // This txn, 0xfeedbf51b4e2338e38171f6e19501327294ab1907ab44cfd2d7e7336c975ace7,
-// incorrectly attributed profit in sDAI due to it. Checking for `Withdraw` to 
+// incorrectly attributed profit in sDAI due to it. Checking for `Withdraw` to
 // address that issue.
 const peculiarTokens = [
   "0x83f20f44975d03b1b09e64809b757c47f942beea", // sDAI on Ethereum mainnet
 ];
 const peculiarEvents = [
-  "Withdraw" // sDAI on Ethereum mainnet
+  "Withdraw", // sDAI on Ethereum mainnet
 ];
+
+const ARB_WETH_ADDRESS = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1";
+
+const etherscanApis = {
+  1: {
+    urlContractCreation: "https://api.etherscan.io/api?module=contract&action=getcontractcreation",
+  },
+  10: {
+    urlContractCreation: "https://api-optimistic.etherscan.io/api?module=contract&action=getcontractcreation",
+  },
+  56: {
+    urlContractCreation: "https://api.bscscan.com/api?module=contract&action=getcontractcreation",
+  },
+  137: {
+    urlContractCreation: "https://api.polygonscan.com/api?module=contract&action=getcontractcreation",
+  },
+  250: {
+    urlContractCreation: "https://api.ftmscan.com/api?module=contract&action=getcontractcreation",
+  },
+  42161: {
+    urlContractCreation: "https://api.arbiscan.io/api?module=contract&action=getcontractcreation",
+  },
+  43114: {
+    urlContractCreation: "https://api.snowtrace.io/api?module=contract&action=getcontractcreation",
+  },
+};
 
 const ethcallProvider = new MulticallProvider(getEthersProvider());
 
@@ -25,9 +51,19 @@ const tokenDecimals = {};
 
 const tokensPriceCache = new LRUCache({ max: 100_000 });
 
+let getApiKeys;
+const MAX_RETRIES = 3;
+
+// Helps to avoid circular dependency issue
+function init() {
+  if (!getApiKeys) {
+    // Require and set getApiKeys if it hasn't been set
+    const agent = require("./agent");
+    getApiKeys = agent.getApiKeys;
+  }
+}
 
 async function getTokenPrice(chain, asset, blockNumber) {
-
   for (let j = blockNumber - 4; j <= blockNumber; j++) {
     const key = `usdPrice-${asset}-${j}`;
     if (tokensPriceCache.has(key)) {
@@ -76,7 +112,6 @@ async function getTokenPrice(chain, asset, blockNumber) {
 }
 
 async function getNativeTokenPrice(chain, blockNumber) {
-
   for (let j = blockNumber - 4; j <= blockNumber; j++) {
     const key = `usdPrice-${chain}-${j}`;
     if (tokensPriceCache.has(key)) {
@@ -214,6 +249,29 @@ function getNativeTokenByChainId(chainId) {
   }
 }
 
+function getBlockExplorerKey(chainId) {
+  init();
+  const apiKeys = getApiKeys();
+  const getKey = (keys) => (keys.length > 0 ? keys[Math.floor(Math.random() * keys.length)] : "YourApiKeyToken");
+
+  switch (chainId) {
+    case 10:
+      return getKey(apiKeys.apiKeys.flashloan.optimisticEtherscanApiKeys);
+    case 56:
+      return getKey(apiKeys.apiKeys.flashloan.bscscanApiKeys);
+    case 137:
+      return getKey(apiKeys.apiKeys.flashloan.polygonscanApiKeys);
+    case 250:
+      return getKey(apiKeys.apiKeys.flashloan.fantomscanApiKeys);
+    case 42161:
+      return getKey(apiKeys.apiKeys.flashloan.arbiscanApiKeys);
+    case 43114:
+      return getKey(apiKeys.apiKeys.flashloan.snowtraceApiKeys);
+    default:
+      return getKey(apiKeys.apiKeys.flashloan.etherscanApiKeys);
+  }
+}
+
 module.exports = {
   zero,
   getTransactionReceipt,
@@ -240,19 +298,18 @@ module.exports = {
 
       // Check for general peculiar tokens before checking for specific tokens
       // since both `peculiarTokens` and `perculiarEvents` can grow.
-      if(peculiarTokens.includes(address) && peculiarEvents.includes(event.name)) {
+      if (peculiarTokens.includes(address) && peculiarEvents.includes(event.name)) {
         // See note above `peculiarToken` declaration on sDAI on ETH mainnet
-        if(address === peculiarTokens[0] && event.name === peculiarEvents[0]) {
+        if (address === peculiarTokens[0] && event.name === peculiarEvents[0]) {
           const { owner, shares } = event.args;
-  
+
           const src = owner?.toLowerCase();
-  
+
           if (src === account) {
             profits[address] = profits[address].sub(shares);
           }
-  
         }
-      } else if(!peculiarEvents.includes(event.name)) {
+      } else if (!peculiarEvents.includes(event.name)) {
         const { src: s, dst: d, wad } = event.args;
 
         // Convert the source and destination addresses to lower case
@@ -330,7 +387,6 @@ module.exports = {
     // Calculate the usd profit based on the amount and the price
     const usdTokenProfits = await Promise.all(
       Object.entries(nonZeroProfits).map(async ([address, profit]) => {
-
         const usdPrice = await getTokenPrice(chain, address, blockNumber);
 
         if (!usdPrice) return 0;
@@ -370,6 +426,49 @@ module.exports = {
 
     const tokenAmount = ethers.utils.formatUnits(amount, tokenDecimals[asset]);
     return tokenAmount * usdPrice;
+  },
+  async getContractCreator(address, chainId) {
+    const { urlContractCreation } = etherscanApis[chainId];
+    const key = getBlockExplorerKey(chainId);
+    const url = `${urlContractCreation}&contractaddresses=${address}&apikey=${key}`;
+
+    for (let attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+      try {
+        const result = await (await fetch(url)).json();
+
+        if (
+          result.message.startsWith("NOTOK") ||
+          result.message.startsWith("No data") ||
+          result.message.startsWith("Query Timeout")
+        ) {
+          console.log(`Block explorer error occurred (attempt ${attempt}); retrying check for ${address}`);
+          if (attempt === MAX_RETRIES) {
+            console.log(`Block explorer error occurred (final attempt); skipping check for ${address}`);
+            return null;
+          }
+        } else return result.result[0].contractCreator;
+      } catch (error) {
+        console.error(`An error occurred during the fetch (attempt ${attempt}):`, error);
+        if (attempt === MAX_RETRIES) {
+          console.error(`Error during fetch (final attempt); skipping check for ${address}`);
+          return null;
+        }
+      }
+    }
+
+    console.error(`Failed to fetch contract creator for ${address} after ${MAX_RETRIES} retries`);
+    return null;
+  },
+  addWethBurnProfitIfApplicable(chainId, transferEvents, calledContract, totalNativeProfit) {
+    // Check if we're on Arbitrum and the last Transfer event is a WETH burn from the called contract
+    if (Number(chainId) === 42161 && transferEvents[transferEvents.length - 1].address === ARB_WETH_ADDRESS) {
+      const { src: s, dst: d, wad } = transferEvents[transferEvents.length - 1].args;
+      if (d === ethers.constants.AddressZero && s.toLowerCase() === calledContract.toLowerCase()) {
+        // If so, add the profit to the totalNativeProfit
+        return totalNativeProfit.add(wad);
+      }
+    }
+    return totalNativeProfit;
   },
   clear() {
     const tokenAddresses = Object.keys(tokenDecimals);
